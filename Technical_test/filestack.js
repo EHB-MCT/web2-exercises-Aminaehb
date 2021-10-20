6c4cff13916db1a1340d964a4ccfa8078b3944a6
Aminaehb <71768425+Aminaehb@users.noreply.github.com>
2021-10-20 13:05:51 +0200

    edited the script + added html

---
+++ Technical_test/filestack.js
@@ -0,0 +1,1 @@
+const client = require('filestack-js').init('apikey');
--- Technical_test/script.js
+++ Technical_test/script.js
@@ -1,19 +1,29 @@
-const client = filestack.init('AxN8ROZz3T8azRC4SxHcQz');
-const api = ('https://www.filestackapi.com/api/file');
-const options = {
-    fromSources: ["local_file_system", "instagram", "facebook"],
-    accept: ["image/*"],
-    uploadConfig: {
-        retry: 5,
-        timeout: 60000
-    },
-    onFileSelected: file => {
-        // If you throw any error in this function it will reject the file selection.
-        // The error message will be displayed to the user as an alert.
-        if (file.size > 1000 * 1000) {
-            throw new Error('File too big, select something smaller than 1MB');
-        }
-    }
-};
+//const api = ('https://www.filestackapi.com/api/file');
 
-client.picker(options).open();
+const client = filestack.init("AxN8ROZz3T8azRC4SxHcQz");
+const picker = client.picker({
+    fromSources: ["local_file_system", "instagram", "facebook"],
+    uploadInBackground: false,
+    accept: ["image/*"],
+    transformations: {
+        crop: false,
+        circle: true,
+        rotate: true
+    },
+    exposeOriginalFile: true,
+
+});
+
+picker.open();
+//import * as filestack from 'filestack-js';
+
+fetch('https://www.filestackapi.com/api/store/S3?key=AxN8ROZz3T8azRC4SxHcQz', {
+        method: 'POST',
+        body: 'formData'
+    }).then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
+    .catch(response => {
+        console.log("error")
+    })
